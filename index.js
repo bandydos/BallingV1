e8913a49585230d4ee8c743650578a5c2ced11b0
KR44I <38465150+AlexaTack@users.noreply.github.com>
2020-02-02 00:22:36 +0100

    Added server communication

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ app.use(express.static('public'));
 app.use(express.static('node_modules/jquery/dist'));
 app.use(express.static('node_modules/bootstrap/dist')); 
 
-app.post('/api', function(req, res){
-    console.log(req); //Request data from client.
+app.use(express.json({limit: '1mb'})); //Needed to able to parse JSON from client.
+app.post('/api', function(req, res){ 
+    console.log("Got a request.")
+    var data = req.body; //Request data (body) from client.
+    console.log(data); 
+    res.json({ //Response to client.
+        status: 's',
+        date: data.date,
+        home: data.home,
+        away: data.away
+    });
 });
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,45 +1,57 @@
 //Client code.
-$(document).ready(function(){
+$(document).ready(function(){ //On ready.
     console.log('Ready.');
-    const url = 'data/bballteams.json';
-    $.getJSON(url, function(data){
-        $(data).each(function(i, team){
-            $("#select-home-team").append
-            ('<option>' + team.name + '</option>');
+    const url = 'data/bballteams.json'; //Url to JSON file.
+    $.getJSON(url, function(data){ //Get JSON AJAX.
+        $(data).each(function(i, team){ //For each.
+            $("#select-home-team").append //Append options 
+            ('<option>' + team.name + '</option>'); //to selects.
             $("#select-away-team").append
             ('<option>' + team.name + '</option>');
         });
     });
-})
+});
 
-function getDate(){
+function getDate(){ //Function to get date.
     var dateIn = document.getElementById("date-in");
-    var date = dateIn.value;
-    return date;
+    var date = dateIn.value; //Get date value.
+    return date; //Return.
 }
 
-function getHomeTeam(){
+function getHomeTeam(){ //Function to get home team.
     var selectHomeTeam = document.getElementById("select-home-team");
     var homeTeam = selectHomeTeam.value;
     return homeTeam;    
 }
 
-function getAwayTeam(){
+function getAwayTeam(){ //Function to get away team.
     var selectAwayTeam = document.getElementById("select-away-team");
     var awayTeam = selectAwayTeam.value;
     return awayTeam;
 }
 
 const btnSend = document.getElementById("btn-send");
-btnSend.onclick = function(){
-    var date = getDate();
-    var home  = getHomeTeam();
-    var away = getAwayTeam();
-    var data = {
+//Had to make this async for await for response.
+btnSend.onclick = async function(){ //When clicking.
+    var date = getDate(); //Date.
+    var home  = getHomeTeam(); //Home team.
+    var away = getAwayTeam(); //Away team.
+    var data = { //Object with all needed data.
         date,
         home,
         away
     };
-    console.log(data);
+
+    var options = { //Options for fetch.
+        method: 'POST', //Sort of method.
+        headers: { //Basic headers.
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data) //Body.
+    }
+
+    var response = await fetch('/api', options); //Await fetch method.
+    var json = await response.json(); //Get respone.json() (zie server).
+    console.log(json);
 }
 
