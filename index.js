a9e4b466b20aec684629f86ad1decf5a8b4fb2ad
KR44I <38465150+AlexaTack@users.noreply.github.com>
2020-02-02 15:36:20 +0100

    Going to first get more understanding on some stuff

--- index.js
+++ index.js
@@ -2,21 +2,32 @@
 const express = require('express'); //Require express.
 const Datastore = require('nedb'); //Require nedb database.
 const app = express();
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log('Listening (3000).');
 });
 
 app.use(express.static('public'));
 app.use(express.static('node_modules/jquery/dist'));
-app.use(express.static('node_modules/bootstrap/dist')); 
+app.use(express.static('node_modules/bootstrap/dist'));
 
-app.use(express.json({limit: '1mb'})); //Needed to able to parse JSON from client.
+app.use(express.json({ limit: '1mb' })); //Needed to able to parse JSON from client.
 const database = new Datastore('database.db'); //Path to my db file.
 database.loadDatabase(); //Load.
 
-app.post('/api', function(req, res){ 
+app.post('/api', function (req, res) { //POST
     console.log("Got a request.")
     var data = req.body; //Request data (body) from client.
     database.insert(data); //Insert in database.
     res.json(data); //Response to client.
+});
+
+app.get('/api', function (req, res) { //GET
+    database.find({}, function (err, data) {
+        if (err) { //If error occurs.
+            res.end(); //End response.
+            return;
+        } else {
+            res.json(data); //Send json response with data.
+        }
+    });
 });
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,40 +1,39 @@
 //Client code.
-$(document).ready(function(){ //On ready.
+$(document).ready(function () { //On ready.
     console.log('Ready.');
-    const url = 'data/bballteams.json'; //Url to JSON file.
-    $.getJSON(url, function(data){ //Get JSON AJAX.
-        $(data).each(function(i, team){ //For each.
+    const url = 'data/teams.json'; //Url to JSON file.
+    $.getJSON(url, function (data) { //Get JSON AJAX.
+        $(data).each(function (i, team) { //For each.
             $("#select-home-team").append //Append options 
-            ('<option>' + team.name + '</option>'); //to selects.
+                ('<option>' + team.name + '</option>'); //to selects.
             $("#select-away-team").append
-            ('<option>' + team.name + '</option>');
+                ('<option>' + team.name + '</option>');
         });
     });
 });
 
-function getDate(){ //Function to get date.
+function getDate() { //Function to get date.
     var dateIn = document.getElementById("date-in");
     var date = dateIn.value; //Get date value.
     return date; //Return.
 }
 
-function getHomeTeam(){ //Function to get home team.
+function getHomeTeam() { //Function to get home team.
     var selectHomeTeam = document.getElementById("select-home-team");
     var homeTeam = selectHomeTeam.value;
-    return homeTeam;    
+    return homeTeam;
 }
 
-function getAwayTeam(){ //Function to get away team.
+function getAwayTeam() { //Function to get away team.
     var selectAwayTeam = document.getElementById("select-away-team");
     var awayTeam = selectAwayTeam.value;
     return awayTeam;
 }
 
 const btnSend = document.getElementById("btn-send");
-//Had to make this async for await for response.
-btnSend.onclick = async function(){ //When clicking.
+btnSend.onclick = async function () { //When clicking.
     var date = getDate(); //Date.
-    var home  = getHomeTeam(); //Home team.
+    var home = getHomeTeam(); //Home team.
     var away = getAwayTeam(); //Away team.
     var data = { //Object with all needed data.
         date,
@@ -50,8 +49,33 @@ btnSend.onclick = async function(){ //When clicking.
         body: JSON.stringify(data) //Body.
     }
 
-    var response = await fetch('/api', options); //Await fetch method.
-    var json = await response.json(); //Get respone.json() (zie server).
-    console.log(json);
+    const errorLabel = document.getElementById('error-label');
+    if ((date != "") && (home != "#") && (away != "#")) { //If fields are not empty.
+        var response = await fetch('/api', options); //Await fetch method.
+        var jsonresponse = await response.json(); //Await respone.json() (zie server).
+        errorLabel.innerHTML = ""; //Clear error label.
+        console.log(jsonresponse);
+    } else {
+        errorLabel.innerHTML = "Please fill in all fields correctly." //Error message.
+        return;
+    }
 }
 
+getData();
+async function getData() {
+    const response = await fetch('/api'); //Await get fetch.
+    const data = await response.json(); //Wait for response from server.
+    const gameTable = document.getElementById("game-table")
+    var i;
+    for (item of data){
+        const row = document.createElement("tr");
+        const colDate = document.createElement("td");
+        const colGame = document.createElement("td");
+
+        colDate.textContent = item.date;
+        colGame.textContent = item.home + " vs " + item.away;
+        row.append(colDate, colGame);
+        gameTable.append(row);
+    }
+    console.log(data);
+}
