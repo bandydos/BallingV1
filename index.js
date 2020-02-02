767ebe08eebc7c2578b632862e38e5013a8ea225
KR44I <38465150+AlexaTack@users.noreply.github.com>
2020-02-02 11:47:05 +0100

    Going to try and clean up code

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //Server code.
-const express = require('express');
+const express = require('express'); //Require express.
+const Datastore = require('nedb'); //Require nedb database.
 const app = express();
 app.listen(3000, function(){
     console.log('Listening (3000).');
@@ -10,14 +11,12 @@ app.use(express.static('node_modules/jquery/dist'));
 app.use(express.static('node_modules/bootstrap/dist')); 
 
 app.use(express.json({limit: '1mb'})); //Needed to able to parse JSON from client.
+const database = new Datastore('database.db'); //Path to my db file.
+database.loadDatabase(); //Load.
+
 app.post('/api', function(req, res){ 
     console.log("Got a request.")
     var data = req.body; //Request data (body) from client.
-    console.log(data); 
-    res.json({ //Response to client.
-        status: 's',
-        date: data.date,
-        home: data.home,
-        away: data.away
-    });
+    database.insert(data); //Insert in database.
+    res.json(data); //Response to client.
 });
